39271966d1dd0efbb4729f3434a77993dc248c9c
Patrik Montada <patrik.montada@googlemail.com>
2020-10-15 14:51:18 +0200

    fix

--- index.js
+++ index.js
@@ -22,34 +22,35 @@ client.once('ready', async () => {
 });
 
 client.on('message', async message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	const { member, channel, content, guild, author } = message
+	if (!content.startsWith(prefix) || author.bot) return;
 
-	let args = message.content.slice(prefix.length).trim().split(/ +/);
+	let args = content.slice(prefix.length).trim().split(/ +/);
 	let command = args.shift().toLowerCase();
 
 	if (command === 'ping') {
-		message.channel.send('Pong!');
+		channel.send('Pong!');
 	}
 
 	if (command === 'invite') {
-		replyWithInvite(message);
+		replyWithInvite(channel);
 	}
 
 	if (command === 'channel') {
-		let channel = getChannelFromMention(args[0])
-		if (channel == null) {
+		let newChannel = getChannelFromMention(args[0])
+		if (newChannel == null) {
 			message.reply("I can't see that channel!");
 			return;
 		} else {
-			cache[message.guild.id] = [channel]
+			cache[guild.id] = newChannel.id
 
 			await mongo().then(async (mongoose) => {
 				try {
 					await claimchannelSchema.findOneAndUpdate({
-						_id: message.guild.id
+						_id: guild.id
 					}, {
-						_id: message.guild.id,
-						channelId: channel,
+						_id: guild.id,
+						channelId: newChannel.id,
 					}, {
 						upsert: true
 					})
@@ -66,60 +67,48 @@ client.on('message', async message => {
 	}
 
 	if (command === 'claim') {
-		let data = cache[message.guild.id]
+		let claimchannelId = cache[guild.id]
 
-		if (data == null) {
-			fetchChannelFromDb(message)
+		if (!claimchannelId) {
+			console.log('FETCHING FROM DATABASE')
+			await mongo().then(async (mongoose) => {
+				try {
+					const result = await claimchannelSchema.findOne({ _id: guild.id })
+
+					cache[guild.id] = claimchannelId = result.channelId
+				} finally {
+					mongoose.connection.close()
+				}
+			});
 		}
 		
-		data = cache[message.guild.id]
-		
-		const channelId = data[0]
-		const claimChannel = message.guild.channels.cache.get(channelId)
+		guild.channels.cache.get(claimchannelId).send("test")
+	}
 
-		claimChannel.send("Posting claims in here. This is a fake claim blabla")
+	function getChannelFromMention(mention) {
+		if (!mention) return;
+
+		if (mention.startsWith('<#') && mention.endsWith('>')) {
+			mention = mention.slice(2, -1)
+
+			return client.channels.cache.get(mention)
+		}
+	}
+
+
+	async function replyWithInvite(channel) {
+		const embedInvite = new Discord.MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle('Invite me!')
+			.setURL('https://discord.com/api/oauth2/authorize?client_id=766223518659772416&permissions=149504&scope=bot')
+			.setAuthor('ISFL Claim Thread Watcher', 'https://i.imgur.com/fPW1MS5.png')
+			.setDescription('A Bot to automatically post the newest claims from the ISFL Claim Thread!')
+			.setThumbnail('https://i.imgur.com/fPW1MS5.png')
+			.setTimestamp()
+			.setFooter('Invite send at: ');
+
+		channel.send(embedInvite);
 	}
 });
-
-function getChannelFromMention(mention) {
-	if (!mention) return;
-
-	if (mention.startsWith('<#') && mention.endsWith('>')) {
-		mention = mention.slice(2, -1)
-
-		if (mention.startsWith('!')) {
-			mention = mention.slice(1);
-		}
-
-		return client.channels.cache.get(mention)
-	}
-}
-
-async function fetchChannelFromDb(message) {
-	console.log('FETCHING FROM DATABASE')
-	await mongo().then(async (mongoose) => {
-		try {
-			const result = await claimchannelSchema.findOne({ _id: message.guild.id })
-
-			cache[message.guild.id] = [result.channelId]
-		} finally {
-			mongoose.connection.close()
-		}
-	});
-}
-
-async function replyWithInvite(message) {
-	const embedInvite = new Discord.MessageEmbed()
-		.setColor('#0099ff')
-		.setTitle('Invite me!')
-		.setURL('https://discord.com/api/oauth2/authorize?client_id=766223518659772416&permissions=149504&scope=bot')
-		.setAuthor('ISFL Claim Thread Watcher', 'https://i.imgur.com/fPW1MS5.png')
-		.setDescription('A Bot to automatically post the newest claims from the ISFL Claim Thread!')
-		.setThumbnail('https://i.imgur.com/fPW1MS5.png')
-		.setTimestamp()
-		.setFooter('Invite send at: ');
-
-	message.channel.send(embedInvite);
-}
 
 client.login(config.token);
