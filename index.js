048eba32446adf9149893e722bd77596f8ab87ec
Patrik Montada <patrik.montada@googlemail.com>
2020-10-15 18:59:15 +0200

    test

--- index.js
+++ index.js
@@ -199,10 +199,10 @@ async function checkThreads() {
 
 	for (const [key, value] of Object.entries(threads)) {
 		console.log(`${key}: ${value}`)
-		console.log(Object.keys(value))
-		console.log(Object.values(value))
-		console.log(Object.keys(key))
-		console.log(Object.values(key))
+		console.log('Object Keys Value' + Object.keys(value))
+		console.log('Object Values Value' + Object.values(value))
+		console.log('Object Keys Key' + Object.keys(key))
+		console.log('Object Values Key' + Object.values(key))
 	}
 
 
